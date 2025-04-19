61df099e5d25c833ec87b11439e528625b3d3b76
Marcin Szafert <mszafert@gmail.com>
2025-04-19 13:58:07 +0200

    UPDATE layout footer and homepage styles

--- uno.config.ts
+++ uno.config.ts
@@ -1,6 +1,16 @@
-import { defineConfig, presetWind3, transformerDirectives, transformerVariantGroup } from 'unocss';
+import {
+  defineConfig,
+  presetWind3,
+  transformerDirectives,
+  transformerVariantGroup,
+} from 'unocss';
 
 export default defineConfig({
   presets: [presetWind3()],
+  theme: {
+    colors: {
+      brand: '#8bbf3e',
+    }
+  },
   transformers: [transformerDirectives(), transformerVariantGroup()],
 });
