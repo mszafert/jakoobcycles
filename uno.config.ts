5b1b87cde4c2f33ba95df179842499aeb83f9c14
Marcin Szafert <mszafert@gmail.com>
2025-05-12 00:17:05 +0200

    ADD Google fonts

--- uno.config.ts
+++ uno.config.ts
@@ -1,12 +1,33 @@
 import {
   defineConfig,
   presetWind3,
+  presetWebFonts,
   transformerDirectives,
   transformerVariantGroup,
 } from 'unocss';
 
 export default defineConfig({
-  presets: [presetWind3()],
+  presets: [
+    presetWind3(),
+    presetWebFonts({
+      provider: 'google',
+      fonts: {
+        lato: [
+          {
+            name: 'Lato',
+            weights: ['300', '400', '700'],
+            italic: true,
+          },
+        ],
+        oswald: [
+          {
+            name: 'Oswald',
+            weights: ['700'],
+          },
+        ],
+      },
+    }),
+  ],
   theme: {
     colors: {
       brand: '#8bbf3e',
